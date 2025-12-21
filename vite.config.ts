b41eb12621239e71865f51e5ad14253ed78eb426
Akshay Saini <akshaymarch7@gmail.com>
2025-12-22 03:03:33 +0530

    chore: Adjust base path and homepage for root domain deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/portfolio-ide/',
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
